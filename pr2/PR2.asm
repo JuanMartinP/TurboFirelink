675fdb7277cfc00d904a4a50c4450f676f358603
JuanMartinP <36308570+JuanMartinP@users.noreply.github.com>
2019-03-19 13:01:06 +0100

    Direccionamiento indexado, muestra 12

--- pr2/PR2.asm
+++ pr2/PR2.asm
@@ -6,8 +6,12 @@
 ; DEFINICION DEL SEGMENTO DE DATOS 
 DATOS SEGMENT 
 ;-- rellenar con los datos solicitados
-	MATRIZ9 DB 9 DUP(0)					;reservamos memoria para 9 valores de 8 bytes
-	RESULTADO DB 1 DUP(2 DUP(0)) 		;reservamos memoria para el resultado de 16 bytes
+	MATRIZ9 DB 1, 2, 3, 4, 5, 2, 2, 4, 2									;reservamos memoria para 9 valores de 1 byte inicializados
+	RESULTADO DB 5 DUP(0) 													;reservamos memoria para el resultado de 16 bytes
+	MATRIZGR DB "     |", 0,0,0, '|', 0,0,0, '|', 0,0,0, '|', 10, 13, '$'
+			 DB "|A|= |", 0,0,0, '|', 0,0,0, '|', 0,0,0, "| =",0,0,0, 10, 13, '$'
+			 DB "     |", 0,0,0, '|', 0,0,0, '|', 0,0,0, '|', 10, 13, '$'		 
+	
 DATOS ENDS 
 ;************************************************************************** 
 ; DEFINICION DEL SEGMENTO DE PILA 
@@ -41,43 +45,36 @@ INICIO PROC
 	;MOV DL, MATRIZ9[0]
 	;int 21H
 	
-	MOV MATRIZ9[0], 1
-	
-	MOV MATRIZ9[1], 2
-	
-	MOV MATRIZ9[2], 3
-	
-	MOV MATRIZ9[3], 4
-	
-	MOV MATRIZ9[4], 5
-	
-	MOV MATRIZ9[5], 2
-	
-	MOV MATRIZ9[6], 2
-	
-	MOV MATRIZ9[7], 4
-
-	MOV MATRIZ9[8], 2
-	
-	
 	;DETERMINANTE
 	
-	;multiplicar primer triplete de la matriz: matriz[0] * matriz[4] * matriz[8] ---> Comprobado
-	MOV AL, MATRIZ9[0]
-	MOV CL, MATRIZ9[4]
+	;multiplicar primer triplete de la matriz: matriz[0] * matriz[4] * matriz[8] (BX = nfila*3)(SI = ncol)
+	MOV BX, 3*0
+	MOV SI, 0
+	MOV AL, MATRIZ9[BX][SI]
+	MOV BX, 3*1
+	MOV SI, 1
+	MOV CL, MATRIZ9[BX][SI]
 	IMUL CL
 	
-	MOV CL, MATRIZ9[8]
+	MOV BX, 3*2
+	MOV SI, 2
+	MOV CL, MATRIZ9[BX][SI]
 	IMUL CL
 	
 	MOV CL, AL
 	
 	;multiplicar segundo triplete de la matriz: matriz[3] * matriz[7] * matriz[2] ---> Comprobado
-	MOV AL, MATRIZ9[3]
-	MOV DL, MATRIZ9[7]
+	MOV BX, 3*1
+	MOV SI, 0
+	MOV AL, MATRIZ9[BX][SI]
+	MOV BX, 3*2
+	MOV SI, 1
+	MOV DL, MATRIZ9[BX][SI]
 	IMUL DL
 	
-	MOV DL, MATRIZ9[2]
+	MOV BX, 3*0
+	MOV SI, 2
+	MOV DL, MATRIZ9[BX][SI]
 	IMUL DL
 	
 	MOV DL, AL
@@ -86,17 +83,22 @@ INICIO PROC
 	;Para comprobar hasta aqui: MOV AL, CL y salida
 	
 	;multiplicar tercer triplete de la matriz: matriz[1] * matriz[5] * matriz[6] ---> A Juan no le apetecia comprobarlo
-	MOV AL, MATRIZ9[1]
-	MOV DL, MATRIZ9[5]
+	MOV BX, 3*0
+	MOV SI, 1
+	MOV AL, MATRIZ9[BX][SI]
+	MOV BX, 3*1
+	MOV SI, 2
+	MOV DL, MATRIZ9[BX][SI]
 	IMUL DL
 	
-	MOV DL, MATRIZ9[6]
+	MOV BX, 3*2
+	MOV SI, 0
+	MOV DL, MATRIZ9[BX][SI]
 	IMUL DL
 	
 	MOV DL, AL
 	;suma de los dos primeros tripletes con el tercero, se guarda en CL
 	ADD CL, DL
-	;Para comprobar hasta aqui: MOV AL, CL y salida
 	
 	;;/*************************************/
 	;;SEGUNDA PARTE DETERMINANTE
@@ -104,46 +106,102 @@ INICIO PROC
 	
 	
 	;multiplicar primer triplete de la matriz: matriz[0] * matriz[4] * matriz[8] ---> Comprobado
-	MOV AL, MATRIZ9[2]
-	MOV BL, MATRIZ9[4]
+	MOV BX, 3*0
+	MOV SI, 2
+	MOV AL, MATRIZ9[BX][SI]
+	MOV BX, 3*1
+	MOV SI, 1
+	MOV BL, MATRIZ9[BX][SI]
 	IMUL BL
 	
-	MOV BL, MATRIZ9[6]
+	MOV BX, 3*2
+	MOV SI, 0
+	MOV BL, MATRIZ9[BX][SI]
 	IMUL BL
 	
-	MOV BL, AL
+	MOV CH, AL
 	
 	;multiplicar segundo triplete de la matriz: matriz[3] * matriz[7] * matriz[2] ---> Comprobado
-	MOV AL, MATRIZ9[3]
-	MOV DL, MATRIZ9[1]
+	MOV BX, 3*1
+	MOV SI, 0
+	MOV AL, MATRIZ9[BX][SI]
+	MOV BX, 3*0
+	MOV SI, 1
+	MOV DL, MATRIZ9[BX][SI]
 	IMUL DL
 	
-	MOV DL, MATRIZ9[8]
+	MOV BX, 3*2
+	MOV SI, 2
+	MOV DL, MATRIZ9[BX][SI]
 	IMUL DL
 	
 	MOV DL, AL
 	;suma de primer y segundo triplete
-	ADD BL, DL
+	ADD CH, DL
 	;Para comprobar hasta aqui: MOV AL, CL y salida
 	
 	;multiplicar segundo triplete de la matriz: matriz[1] * matriz[5] * matriz[6] ---> A Juan no le apetecia comprobarlo
-	MOV AL, MATRIZ9[7]
-	MOV DL, MATRIZ9[5]
+	MOV BX, 3*2
+	MOV SI, 1
+	MOV AL, MATRIZ9[BX][SI]
+	MOV BX, 3*1
+	MOV SI, 2
+	MOV DL, MATRIZ9[BX][SI]
 	IMUL DL
 	
-	MOV DL, MATRIZ9[0]
+	MOV BX, 3*0
+	MOV SI, 0
+	MOV DL, MATRIZ9[BX][SI]
 	IMUL DL
 	
 	MOV DL, AL
-	ADD BL, DL
+	ADD CH, DL
 	;Para comprobar hasta aqui: MOV AL, CL y salida
-	SUB CL, BL
-
-	MOV AL, CL
+	SUB CL, CH
 	
-	MOV AH, 2
+	; tenemos el determinante en CL y lo vamos a pasar a ascii para imprimir el valor correcto
+	
+	
+	;MOV DX, OFFSET RESULTADO
+	;MOV AH, 9
+	;INT 21h
+
+	;MOV AH, 2 					;Impresion de 1 caracter
+	;MOV DL, AL
+	;int 21H
+	
+	;tenemos un 12 de resultado
+	
+	MOV AL, CL
+	MOV AH, 00
+	MOV BL, 10
+	DIV BL 			;el resto esta en AH
+	ADD AH, 48
+	MOV RESULTADO[1], AH
+	MOV AH, 00
+	DIV BL 			;el resto esta en AH
+	ADD AH, 48
+	MOV RESULTADO[0], AH
+	
+	MOV AL, RESULTADO[0]
+	MOV AH, 2 			
 	MOV DL, AL
 	int 21H
+	
+	MOV AL, RESULTADO[1]
+	MOV AH, 2 			
+	MOV DL, AL
+	int 21H
+	
+	;MOV CH, 13
+	;MOV RESULTADO[2], CH
+	;MOV CH, 10
+	;MOV RESULTADO[3], CH
+	;MOV CH, '$'
+	;MOV RESULTADO[4], CH
+
+	
+	
 	
 	
 ; FIN DEL PROGRAMA 
