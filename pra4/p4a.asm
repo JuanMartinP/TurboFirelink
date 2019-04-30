b006ad3d9b44e6036a2c1fbddddc2f5c2357740f
JuanMartinP <36308570+JuanMartinP@users.noreply.github.com>
2019-04-30 19:42:23 +0200

    Bien comentadito

--- pra4/p4a.asm
+++ pra4/p4a.asm
@@ -23,13 +23,13 @@ INICIO:
 	RESCOL DB 0
 	POLIBIO DB "333225221122320202025253$"
 	
-	RSI_57H PROC FAR				;ESTO SE VA A ENCARGAR DE CODIFICAR Y DECODIFICAR LA CADENA QUE LE PASA EL MAIN 
+	RSI_57H PROC FAR				;Codifica y decodifica la cadena en DX 
 		
 		PUSH AX BX CX DX SI DI DS ES 
 		
 		MOV SI, DX
 		
-		MOV AH, 11H						;; Por ahora sólo codifica BIEN, 
+		MOV AH, 11H						;;Para codificar o decodificar 
 		CMP AH, 10H						;; Para codificar	
 		JE CODIFICAR
 		RETCODIFICAR:
@@ -40,34 +40,41 @@ INICIO:
 		
 		JMP FINAL
 		
+		
+		;                     ************************                     ;
+		;*********************Funcion para decodificar**********************;
+		;                     ************************                     ;
 		CODIFICAR:
 		
 			BUCLE:
-			MOV BL, [SI]             ;Cadena almacenada en DS:DX
-			CMP BL, 'A'
+			MOV BL, [SI]             	;Cadena almacenada en DS:DX
+			CMP BL, 'A'					;Si su valos ascii es menos que A es que es un numero
 			JB NUMEROS
 
-			ADD BL, OFFSET_A	
-			MOV RES, BL			
-			SUB RES, 'A'		
+			ADD BL, OFFSET_A			;Offset de A dentro de nuestra tabla de polibio
+			MOV RES, BL					
+			SUB RES, 'A'				;Le restamos el valor ascii de A para ver que letra es	
 			MOV DH, RES			
-			MOV OFFSET_C, DH
-			RETNUMEROS:              ;offest_c = 4
-			JMP PRINT
+			MOV OFFSET_C, DH			
+			RETNUMEROS:              
+			JMP PRINT					;Llamamos a la función para imprimir
 			RETPRINT:
 			INC SI
-			CMP BYTE PTR [SI], '$'
-			JNE BUCLE
+			CMP BYTE PTR [SI], '$'		;Comparamos con $
+			JNE BUCLE					;Si es diferente saltamos al inicio del bucle, si no termina
 
 		JMP RETCODIFICAR
 		
 		
+		;                     *********************                     ;
+		;*********************Lo leido es un numero**********************;
+		;                     *********************                     ;  
 		NUMEROS:
-			CMP BL, '4'
-			JB NUMFINAL
+			CMP BL, '4'					;Comparamos el cacter leido con 4
+			JB NUMFINAL					;Si es menor está en el final de la tabla, si no está al principio
 			
 			MOV RES, BL			
-			SUB RES, '4'		
+			SUB RES, '4'				;Calculamos su posicion en la tabla (4 es el primer num de la tabla)
 			MOV DH, RES			
 			MOV OFFSET_C, DH
 			
@@ -76,67 +83,74 @@ INICIO:
 		JMP RETNUMEROS
 		
 		
+		
+		;                     ****************************************                     ;
+		;*********************Numeros del final de la tabla de polibio**********************;
+		;                     ****************************************                     ;
 		NUMFINAL:
-			ADD BL, OFFSET_0	
+			ADD BL, OFFSET_0			;Posicion del 0 en la tabla
 			MOV RES, BL			
-			SUB RES, '0'		
+			SUB RES, '0'				;Para convertir de su valor ascii a decimal y operar
 			MOV DH, RES			
 			MOV OFFSET_C, DH
 		
 		JMP RETNUMFINAL
-			
-			
+		
+		
+		;                     ************************                     ;
+		;*********************Funcion para decodificar**********************;
+		;                     ************************                     ;
 		DECODIFICAR:
 			BUCLE2:
-			MOV BL, [SI] 
-			SUB BL, 48
+			MOV BL, [SI] 					;Contenido de la cadena a decodificar
+			SUB BL, 48						;Lo convertimos de valor ascii a decimal
 			XOR AH, AH
-			MOV AL, 6
-			MUL BL							;En AL tengo el resultado de 3 * 6 = 18
+			MOV AL, 6						;Filas totales
+			MUL BL							;En AL se queda la fila * filas totales
 			INC SI
-			MOV BL, [SI]
-			SUB BL, 48			
-			ADD BL, AL						
-			XOR BH, BH
-			MOV DI, BX
+			MOV BL, [SI]					;Le sumamos el siguiente numero
+			SUB BL, 48						;Lo convertimos de valor ascii a decimal
+			ADD BL, AL						;fila * filstotales + columna
+			XOR BH, BH						
+			MOV DI, BX						;Su posicion en la tabla
 			
-			MOV AH, 2H
-			MOV DL, DECOD[DI]				
+			MOV AH, 2H						
+			MOV DL, DECOD[DI]				;Imprimimos el caracter decodificado
 			INT 21H
 			INC SI
 			
-			CMP BYTE PTR [SI], '$'
+			CMP BYTE PTR [SI], '$'			;Si no es el fin de la cadena salta al inicio del bucle
 			JNE BUCLE2
 			
 		JMP RETDECODIFICAR
 	
 	
 		PRINT:
-			MOV AL, OFFSET_C		
+			MOV AL, OFFSET_C				;Movemos el caracter decimal a AL
 			MOV AH, 0
 			MOV BH, 6			
-			DIV BH
-			MOV RESFIL, AL				
-			ADD RESFIL, '0'					
+			DIV BH							;Dividimos el caracter decimal entre el numFilas
+			MOV RESFIL, AL					;Cogemos el cociente (que es la fila)
+			ADD RESFIL, '0'					;Lo convertimos a valor ascii
 			MOV DH, RESFIL		
 			MOV FIL, DH						
 			
-			MOV RESCOL, AH
-			ADD RESCOL, '0'					
+			MOV RESCOL, AH					;Cogemos el resto (que es la columna)
+			ADD RESCOL, '0'					;Lo convertimos a valor ascii
 			MOV DH, RESCOL		
 			MOV COL, DH		
 			
 			
 			MOV AH, 2H
-			MOV DL, FIL						
+			MOV DL, FIL						;Imprimimos la fila
 			INT 21H
 				
 			MOV AH, 2H
-			MOV DL, COL						
+			MOV DL, COL						;Imprimimos la columna
 			INT 21H
 			
 			MOV AH, 2H
-			MOV DL, 32						
+			MOV DL, 32						;Imprimimos un espacio
 			INT 21H
 				
 		JMP RETPRINT
@@ -149,7 +163,7 @@ INICIO:
 
 
 	
-	INSTALADOR PROC			;ESTO ES ESTANDAR PARA LAS INTERRUPCIONES
+	INSTALADOR PROC						
 		mov ax, 0
 		mov es, ax
 		mov ax, OFFSET RSI_57H
@@ -159,9 +173,28 @@ INICIO:
 		mov es:[ 40h*4+2 ], bx
 		sti
 		mov dx, OFFSET INSTALADOR
-		int 27h ;  Acaba y deja residente
-		;  PSP, variables y rutina rsi.
+		int 27h 						; Acaba y deja residente
+										;  PSP, variables y rutina rsi.
 	INSTALADOR ENDP
+	
+	
+	desinstalar_40h PROC       			; Desinstala RSI de INT 40h
+		mov cx, 0
+		mov ds, cx						; Segmento de vectores interrupción
+		mov es, ds:[ 40h*4+2 ]      	; Lee segmento de RSI
+		mov bx, es:[ 2Ch ]  			; Lee segmento de entorno del PSP de RSI
+		mov ah, 49h 
+		int 21h							; Libera segmento de RSI (es)
+		mov es, bx
+		int 21h 						; Libera segmento de variables de entorno de RSI
+										; Pone a cero vector de interrupción 40h
+		cli
+		mov ds:[ 40h*4 ], cx 			; cx = 0
+		mov ds:[ 40h*4+2 ], cx
+		sti 
+		ret 
+	desinstalar_40h ENDP
+	
 	
 	;*************************************************************************************************************************;
 	;***************************************************MAIN******************************************************************;
